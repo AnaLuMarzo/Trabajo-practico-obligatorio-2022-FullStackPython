a3d37acea2d2ecc64ab72a9d9a04ab83aad6ac44
AnaLuMarzo <78816332+AnaLuMarzo@users.noreply.github.com>
2022-05-22 22:23:13 -0300

    Add files via upload
    Hice 3 archivos de html, en uno de ellos hice el formulario que lo hice con Bootstrap, tratè de que la barra de navegación coincida con la que hice usando los estilos de forma "manual" pero no me salió exactamente igual. Me costó mucho la parte de validación con JS. En el archivo principal (main.html) coloqué una animación de fotos que van pasando de a una. Para la ubicación de los distintas secciones utilicé todo flexbox, lo utilicé en todas las tarjetas. Encontré una forma de hacer tarjetas que también lo agregué a la pag web.

---
+++ JS/main.js
@@ -0,0 +1,32 @@
+var nombre= document.getElementById('name');
+var email=document.getElementById('email');
+var error=document.getElementById('error');
+error.style.color='red'
+
+function myFunction() {
+    // Toma el valor del campo input con el id="number"
+    let x = document.getElementById("number").value;
+    // Si x no es un numero o no es menor a 120 o no es mayor a 15, es un numero invalido
+    let text;
+
+    if (isNaN(x) || x < 120 || x > 15) {
+      text = "Ingreso no valido";
+
+    } else {
+      text = "Ingreso OK";
+    }
+    document.getElementById("demo").innerHTML = text;
+  
+    var mensajesError=[];
+
+    if (nombre.value===null || nombre.value===''){
+       mensajesError.push('Ingresa tu nombre!');
+    }
+    if (email.value===null || email.value===''){
+      mensajesError.push('Ingresa tu email!');
+   }
+    error.innerHTML= mensajesError.join(' ,');
+    
+    return false;  
+  }
+
